62264956da1d4be8acce031d9737e38f595b2989
okistaylor <72969270+TaylorOkis@users.noreply.github.com>
2025-08-14 14:32:26 +0100

    Add company schema in database

---
+++ prisma/migrations/20250814133022_init_company/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "public"."Company" (
+    "id" TEXT NOT NULL,
+    "companyName" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company_companyName_key" ON "public"."Company"("companyName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company_email_key" ON "public"."Company"("email");
